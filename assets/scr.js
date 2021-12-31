6212f32878fbbf92cb0cfcedb714006d88a13921
volker1 <73304952+REALERvolker1@users.noreply.github.com>
2021-12-31 17:58:19 -0600

    Navbar updates, code optimization
    changelog 1 in readme.md

--- assets/scr.js
+++ assets/scr.js
@@ -2,27 +2,33 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
-var ns=setInterval(navbarshrink,10);
-
+var resized=0;
+var ns=setInterval(navbarshrink,100);
 function navbarshrink(){
 	if (window.innerWidth<=1000 | window.innerHeight<=650){
 		document.getElementById("navbarbg").style.display='none';
 		document.getElementById("mininavswitch").style.display='block';
+		resized=1;
 	}else{
 		document.getElementById("navbarbg").style.display='block';
 		document.getElementById("mininavswitch").style.display='none';
 		document.getElementById("mininav").style.display='none';
+		resized=0;
 	}
 }
-//separate func because I want to be able to add more shti later
-function mininav(){
-	document.getElementById("mininav").style.display='block';
-	document.getElementById("mininavswitch").style.transform='scale(0,0)';
+function menuclicked(){
+	//idfk how mininav is defined already, it's literal black magic
+  if (mininav.style.display=='none'){
+    mininav.style.display='block';
+	
+  }else{
+    mininav.style.display='none';
+  }
 }
-function mininavoff(){//current known bug: Scrolls to the top of the page on scale
-	document.getElementById("mininav").style.display='none';
-	document.getElementById("mininavswitch").style.transform='scale(1,1)';
-}
+var head=document.getElementById("head");
+var headstyle1="display:flex;justify-content:center;align-items:center;height:70px;color:white;background-color:var(--transhigh);backdrop-filter:blur(10px);font-family:'Ubuntu',sans-serif;max-width:800px;min-width:200px;border-radius:var(--rad);margin:auto;margin-top:10px;z-index:8;";
+var headstyle2="position:fixed;top:0px;left:0px;right:0px;display:flex;justify-content:center;align-items:center;height:70px;color:white;background-color:var(--transhigh);backdrop-filter:blur(10px);font-family:'Ubuntu',sans-serif;z-index:8;";
+/*
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
@@ -33,5 +39,14 @@ function scrollFunction() {
     document.getElementById("headscr").style.display='none';
 	document.getElementById("head").style.display='block';
 	document.getElementById("headpush").style.display='none';
+  }
+}*/
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+	
+	head.style=headstyle2;
+  } else {
+	head.style=headstyle1;
   }
 }
