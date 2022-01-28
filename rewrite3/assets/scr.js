8dc057674f876d24a2f833cf997f3b8acaf861ac
volker1 <73304952+REALERvolker1@users.noreply.github.com>
2022-01-28 09:00:36 -0600

    testing commit with vscode

--- rewrite3/assets/scr.js
+++ rewrite3/assets/scr.js
@@ -1,1 +1,14 @@
 //site by volker1
+const nav = document.getElementById('nav');
+var ntg = 0;
+navtoggle = () => {
+    if (ntg == 0){
+        nav.style.display='flex';
+        ntg = 1;
+    }
+    else{
+        nav.style.display='none';
+        ntg = 0;
+    }
+
+}
