5535d5f623ea007e9e85c1e7968625aab4de9fb2
volker1 <73304952+REALERvolker1@users.noreply.github.com>
2022-02-22 12:13:58 -0600

    leadership mobile compat, gallery scaling, js standards compliance

--- assets/debug_screen_size.js
+++ assets/debug_screen_size.js
@@ -1,8 +1,8 @@
 var newHTML = document.createElement ('div');
 newHTML.innerHTML = "             \
-    <div style='position:fixed;top:0px;right:0px;display:block;z-index:1000;height:2em;width:5em;'>             \
-        <p id='screensize'>size</p>       \
-    </div>                          \
+  <div style='position:fixed;top:0px;right:0px;display:block;z-index:1000;height:2em;width:5em;'>             \
+  <p id='screensize'>size</p>       \
+  </div>                          \
 ";
 
 document.body.appendChild (newHTML);
