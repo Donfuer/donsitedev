bd86bb2c16101a859d7499a5ab29f2d1dcc22dc9
volker1 <73304952+REALERvolker1@users.noreply.github.com>
2022-02-25 16:46:08 -0600

    fix

--- assets/debug_screen_size.js
+++
@@ -1,9 +0,0 @@
-var newHTML = document.createElement ('div');
-newHTML.innerHTML = "             \
-  <div style='position:fixed;top:0px;right:0px;display:block;z-index:1000;height:2em;width:5em;'>             \
-  <p id='screensize'>size</p>       \
-  </div>                          \
-";
-
-document.body.appendChild (newHTML);
-var x = setInterval(()=>{document.getElementById('screensize').innerHTML=window.innerWidth;},100);
