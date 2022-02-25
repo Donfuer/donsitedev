a5f252c2c51feb1e939ca0b78c3f81212da6501f
volker1 <73304952+REALERvolker1@users.noreply.github.com>
2022-02-25 09:12:46 -0600

    fixed some outdated css

--- assets/apply/appscr.js
+++ assets/apply/appscr.js
@@ -1,40 +1,31 @@
-//URLs of the apply pages
-const url2B = "https://docs.google.com/forms/d/e/1FAIpQLSd6j8_tkTxyQ8u5NwR_S6G6GGWo5L2Z1gtr7BxXUl3CEtg2oQ/viewform?embedded=true"
-const urlPA = "https://docs.google.com/forms/d/e/1FAIpQLSctbAXk7V1fLgSIhB9Di_oH_PQOlOEGC9cNt2VL7V55_36HDA/viewform?embedded=true"
-const urlSW = "https://docs.google.com/forms/d/e/1FAIpQLSdZS3ppnm1n2cmFnEjZqqRmXKhNESbBYpvrMrYKk8d-nRp4AQ/viewform?embedded=true"
-const url9B = "https://docs.google.com/forms/d/e/1FAIpQLSfj0F5rn6dvrEVnnZOP5qW898CTCRoZ9ZDSBtkAAp1-icx77w/viewform?embedded=true"
-
+const frameArr = ["https://docs.google.com/forms/d/e/1FAIpQLSd6j8_tkTxyQ8u5NwR_S6G6GGWo5L2Z1gtr7BxXUl3CEtg2oQ/viewform?embedded=true",
+"https://docs.google.com/forms/d/e/1FAIpQLSctbAXk7V1fLgSIhB9Di_oH_PQOlOEGC9cNt2VL7V55_36HDA/viewform?embedded=true",
+"https://docs.google.com/forms/d/e/1FAIpQLSdZS3ppnm1n2cmFnEjZqqRmXKhNESbBYpvrMrYKk8d-nRp4AQ/viewform?embedded=true",
+"https://docs.google.com/forms/d/e/1FAIpQLSfj0F5rn6dvrEVnnZOP5qW898CTCRoZ9ZDSBtkAAp1-icx77w/viewform?embedded=true",
+]
 const appFrame = document.getElementById('appframe')
 const app = document.getElementById('app')
 const bRow = document.getElementById("bRow")
-apply = (branch) => {
-  //something here about setting height to be something
-  if (branch == "2B"){
-    app.src = url2B
-  }
-  else if (branch == "PA"){
-    app.src = urlPA
-  }
-  else if (branch == "SW"){
-    app.src = urlSW
-  }
-  else if (branch == "9B"){
-    app.src = url9B
-  }
 
+var urla = window.location.href.toString()
+urla = urla.slice((urla.lastIndexOf("#")+1),urla.length)
+if (urla.length == 1){
+  apply(urla)
+}
+function apply(b){
+  app.src = frameArr[b]
   document.getElementById("appmenu").style.display = "none"
   document.getElementById("appwin").style.display = "block"
   appFrame.style.display = "block"
   app.style.display = "block"
   bRow.style.display = "flex"
 }
-
 function topApp(){
   appFrame.style.display = "none"
   var e = setTimeout(()=>{appFrame.style.display="block"},1)
 }
 function shareApp(){
-  const message1212 = "Press OK to copy the URL to the clipboard - https://www.donfuer.com/apply"
+  const message1212 = "Press OK to copy the URL to the clipboard"
   if (confirm(message1212)){
     const ctxt = document.getElementById("url_to_copy")
     alert("Copied the URL to your clipboard")
