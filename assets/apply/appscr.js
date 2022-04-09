8c11b80baa9c182484fae0d1726052993a7e95a1
volker1 <73304952+REALERvolker1@users.noreply.github.com>
2022-04-09 17:47:37 -0500

    I am inedible

--- assets/apply/appscr.js
+++ assets/apply/appscr.js
@@ -1,7 +1,6 @@
 /*site by volker1*/
 const frameArr = ["https://docs.google.com/forms/d/e/1FAIpQLSd6j8_tkTxyQ8u5NwR_S6G6GGWo5L2Z1gtr7BxXUl3CEtg2oQ/viewform?embedded=true",
 "https://docs.google.com/forms/d/e/1FAIpQLSctbAXk7V1fLgSIhB9Di_oH_PQOlOEGC9cNt2VL7V55_36HDA/viewform?embedded=true",
-"https://docs.google.com/forms/d/e/1FAIpQLSdZS3ppnm1n2cmFnEjZqqRmXKhNESbBYpvrMrYKk8d-nRp4AQ/viewform?embedded=true",
 "https://docs.google.com/forms/d/e/1FAIpQLSfj0F5rn6dvrEVnnZOP5qW898CTCRoZ9ZDSBtkAAp1-icx77w/viewform?embedded=true",
 ]
 //T
