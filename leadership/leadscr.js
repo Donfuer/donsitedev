a59b2a74977cd79bb0a4b770453af830091390f3
REALERvolker1 <73304952+REALERvolker1@users.noreply.github.com>
2022-07-01 01:51:54 -0500

    added myself to leadership
    https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fhumornama.com%2Fwp-content%2Fuploads%2F2021%2F01%2FObama-Awarding-Obama-meme-template.jpg&f=1&nofb=1

--- leadership/leadscr.js
+++ leadership/leadscr.js
@@ -18,14 +18,15 @@ hide = () => {
     win.style.display="none";
     winblur.style.display="none";
 }
-const names = ["D_loaded","DirectorJohnson","Dragonworm","iAmZeiss","TimeOfWheel"];
-const nicks = ["D_loaded","DirectorJohnson","Dragonworm","Zeiss","MusicMaestro"];
+const names = ["D_loaded","DirectorJohnson","Dragonworm","iAmZeiss","TimeOfWheel","volker1"];
+const nicks = ["D_loaded","DirectorJohnson","Dragonworm","Zeiss","MusicMaestro","volker1"];
 const blurbs = ["In August of 2013, D_loaded logged on to his new friend Ben\'s Minecraft server. It was here that he built the first Donfuer. The city was immediately griefed but the name, a supposed mistranslation of \"Father\'s Gift\", stuck and became the name for all the large bases made after. The Donfuerian empire grew slowly over the years as cities rose and fell on many different servers. D_loaded still is in Donfuer today, as its longest serving member.",
 "In April of 2017, a personal friend of MikelAx7 showed him videos about 2b2t. He was mildly interested but dismissive. 3 hours of playtime and 11 hours of queue later, he was hooked. After showing dragonworm the server weeks later, he followed him to Highland and later Donfuer. His membership record was spotty but now he fully committed his playtime to the success of Donfuer. He also created the former DonFuer website and is a developer of the DonHack client. He recently changed his IGN to DirectorJohnson.",
 "Dragonworm got Minecraft when he was a kid in 2010. He learned about 2b2t from his IRL friend MikelAx7, both of them joining the group Highland. Things would quickly sour with Highland, and soon after he joined Donfuer. Dragonworm quickly became an elder after joining by proving his loyalty to D_loaded in a series of crusades on MozartRealms. Since then, he has managed the online presence of Donfuer. He is currently the 3rd oldest Donfuer member that is still around.",
 "Zeiss joined 2b2t in mid 2020, soon joining a small group by the name of Crimson Star. He would build at many bases there, including Cumzone 2 and Adamantium. Zeiss would eventually become a leader, but then Crimson Star would fall inactive, and dissolve. Around this time, he joined Donfuer, and quickly rose up the ranks. He would merge his other group, Fate, into the Donfuer Rangers. After a stint with bad leaders within the Rangers, Zeiss would be offered the position of elder, which he stepped up and took, becoming the newest elder in Donfuer.",
-"TimeOfWheel joined Donfuer in April 2020 on a server called DMC. After that server closed the branch moved to NetherAnarchy. Following NetherAnarchy’s subsequent closure TimeOfWheel created the Phoenix Anarchy branch and is currently leading that branch.",
+"TimeOfWheel joined Donfuer in April 2020 on a server called DMC. After that server closed the branch moved to NetherAnarchy. Following NetherAnarchy's subsequent closure TimeOfWheel created the Phoenix Anarchy branch and is currently leading that branch.",
+"After a couple years of dormancy on 2b2t, volker1 decided to return and \"actually escape spawn this time for realz\". He applied to DonFuer's newly created Spawn Rangers group and made many friends, both in the group and out. He was very active, and was invited to DonFuer 21 with a couple friends. D_loaded eventually offered him leadership of Spawn Rangers, which he accepted. He set to work improving the group by building a new website from the ground up, getting more high-ranking Donfuerians active in Rangers, and more.",
 ];
 const namemcs = ["https://mine.ly/D_loaded.1","https://mine.ly/DirectorJohnson.1","https://mine.ly/dragonworm.1",
-"https://mine.ly/iAmZeiss.1","https://mine.ly/TimeofWheel.1",
+"https://mine.ly/iAmZeiss.1","https://mine.ly/TimeofWheel.1","https://namemc.com/profile/volker1.2",
 ];
